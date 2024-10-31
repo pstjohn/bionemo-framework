5f8880f0536a61816c6c10a885c60ade8056b1c5
sichu <sichu@nvidia.com>
2024-10-31 14:31:36 +0000

    reuse TEDotProductAttention __init__

--- sub-packages/bionemo-esm2/src/bionemo/esm2/model/attention.py
+++ sub-packages/bionemo-esm2/src/bionemo/esm2/model/attention.py
@@ -14,23 +14,16 @@
 # limitations under the License.
 
 
-import os
 from typing import Callable, Optional, Sequence, Union
 
 import torch
 from megatron.core import parallel_state, tensor_parallel
 from megatron.core.extensions.transformer_engine import TEDotProductAttention
 from megatron.core.packed_seq_params import PackedSeqParams
-from megatron.core.parallel_state import (
-    get_context_parallel_global_ranks,
-    get_context_parallel_group,
-    get_tensor_model_parallel_group,
-)
-from megatron.core.tensor_parallel import get_cuda_rng_tracker
 from megatron.core.transformer.dot_product_attention import DotProductAttention
 from megatron.core.transformer.enums import AttnMaskType
 from megatron.core.transformer.transformer_config import TransformerConfig
-from megatron.core.utils import get_te_version, is_te_min_version
+from megatron.core.utils import is_te_min_version
 from torch import Tensor
 
 
@@ -45,97 +38,17 @@ class ESM2TEDotProductAttention(TEDotProductAttention):
 
     def __init__(
         self,
-        config: TransformerConfig,
-        layer_number: int,
-        attn_mask_type: AttnMaskType,
-        attention_type: str,
-        attention_dropout: float | None = None,
+        *args,
         softmax_scale: float = 1.0,
-        k_channels: int | None = None,
-        v_channels: int | None = None,
+        **kwargs,
     ):
         """Initialize ESM2TEDotProductAttention."""
-        self.config = config
-        self.te_forward_mask_type = False
-        self.qkv_format: str = "sbhd"
-
-        if self.config.apply_query_key_layer_scaling != bool(int(os.getenv("NVTE_APPLY_QK_LAYER_SCALING", "0"))):
-            raise ValueError(
-                f"apply_query_key_layer_scaling is {self.config.apply_query_key_layer_scaling} "
-                f"but environment variable NVTE_APPLY_QK_LAYER_SCALING is "
-                f"{os.getenv('NVTE_APPLY_QK_LAYER_SCALING')}. Transformer Engine does not support "
-                f"setting query key layer scaling via argument, so these two must match."
+        if not is_te_min_version("1.10.0"):
+            raise NotImplementedError(
+                "We can reuse __init__ only if TE version is greater than 1.10.0. Otherwise softmax_scale will be ignored. See https://github.com/NVIDIA/Megatron-LM/blob/main/megatron/core/extensions/transformer_engine.py#L620"
             )
 
-        extra_kwargs = {}
-        if is_te_min_version("0.11.0"):
-            extra_kwargs["num_gqa_groups"] = self.config.num_query_groups
-        elif self.config.num_query_groups != self.config.num_attention_heads:
-            raise ValueError(
-                f"Transformer Engine v{get_te_version()} does not support Grouped Query Attention, "
-                f"use a newer version of Transformer Engine. "
-                f"(num_query_groups ({self.config.num_query_groups}) != "
-                f"num_attention_heads ({self.config.num_attention_heads}))"
-            )
-
-        if is_te_min_version("0.10.0"):
-            extra_kwargs["attention_type"] = attention_type
-            # older version don't need attention_type
-
-        if is_te_min_version("0.12.0", check_equality=False):
-            self.te_forward_mask_type = True
-
-        # Only Transformer-Engine version >= 1.0.0 supports context parallelism
-        if is_te_min_version("1.0.0"):
-            if getattr(TEDotProductAttention, "cp_stream") is None:
-                TEDotProductAttention.cp_stream = torch.cuda.Stream()
-            extra_kwargs["cp_group"] = get_context_parallel_group(check_initialized=False)
-            extra_kwargs["cp_global_ranks"] = get_context_parallel_global_ranks(check_initialized=False)
-            extra_kwargs["cp_stream"] = TEDotProductAttention.cp_stream
-        else:
-            assert (
-                self.config.context_parallel_size == 1
-            ), "Only Transformer-Engine version >= 1.0.0 supports context parallelism!"
-
-        if self.config.deterministic_mode:
-            if int(os.getenv("NVTE_ALLOW_NONDETERMINISTIC_ALGO", "1")) != 0:
-                raise RuntimeError(
-                    "deterministic_mode is on and we are using DotProductAttention from "
-                    "Transformer Engine, but NVTE_ALLOW_NONDETERMINISTIC_ALGO is not 0. "
-                    f"Currently set to: {os.getenv('NVTE_ALLOW_NONDETERMINISTIC_ALGO', 'not set')}."
-                )
-
-        if config.window_size is not None:
-            # Check version
-            assert is_te_min_version("1.2.0"), (
-                f"Transformer-Engine v{get_te_version()} must be >= 1.2.0 to support" "sliding window attention."
-            )
-            extra_kwargs["window_size"] = config.window_size
-
-        if is_te_min_version("1.10.0"):
-            # TE 1.10.0 introduces the ability to set the different k and v channels
-            kv_channels = (
-                (k_channels, v_channels)
-                if k_channels is not None and v_channels is not None
-                else self.config.kv_channels
-            )
-        else:
-            kv_channels = self.config.kv_channels
-
-        extra_kwargs["softmax_scale"] = softmax_scale
-
-        super(TEDotProductAttention, self).__init__(
-            num_attention_heads=self.config.num_attention_heads,
-            kv_channels=kv_channels,
-            attention_dropout=(self.config.attention_dropout if attention_dropout is None else attention_dropout),
-            attn_mask_type=attn_mask_type.name,
-            sequence_parallel=self.config.sequence_parallel,
-            tp_size=self.config.tensor_model_parallel_size,
-            get_rng_state_tracker=(get_cuda_rng_tracker if get_cuda_rng_tracker().is_initialized() else None),
-            tp_group=get_tensor_model_parallel_group(check_initialized=False),
-            layer_number=layer_number,
-            **extra_kwargs,
-        )
+        super().__init__(*args, softmax_scale=softmax_scale, **kwargs)
 
 
 class ESM2DotProductAttention(DotProductAttention):
