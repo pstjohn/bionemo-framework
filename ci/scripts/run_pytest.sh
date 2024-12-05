5750373760ab2a71d3acf1e006f430c32fc97b78
Peter St. John <pstjohn@nvidia.com>
2024-12-04 18:55:47 -0800

    add pytest config for codecov

--- ci/scripts/run_pytest.sh
+++ ci/scripts/run_pytest.sh
@@ -25,13 +25,29 @@ if ! set_bionemo_home; then
     exit 1
 fi
 
-python -m coverage erase
+# download Codecov CLI
+curl -Os https://cli.codecov.io/latest/linux/codecov
+
+# integrity check
+curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --no-default-keyring --keyring trustedkeys.gpg --import # One-time step
+curl -Os https://cli.codecov.io/latest/linux/codecov
+curl -Os https://cli.codecov.io/latest/linux/codecov.SHA256SUM
+curl -Os https://cli.codecov.io/latest/linux/codecov.SHA256SUM.sig
+gpgv codecov.SHA256SUM.sig codecov.SHA256SUM
+
+shasum -a 256 -c codecov.SHA256SUM
+sudo chmod +x codecov
 
 for dir in docs/ ./sub-packages/bionemo-*/; do
     echo "Running pytest in $dir"
-    python -m coverage run --parallel-mode --source bionemo \
-        -m pytest -v --nbval-lax --durations=0 --durations-min=60.0 $dir
+    pytest -v --nbval-lax --cov=bionemo --cov-append --junitxml=$(basename $dir).junit.xml -o junit_family=legacy $dir
 done
 
-python -m coverage combine
-python -m coverage report --show-missing
+# Merge all sub-directory test results into a single xml file.
+junitparser merge *.junit.xml combined.junit.xml
+
+# Upload test analytics to codecov.
+./codecov do-upload --report-type test_results --file combined.junit.xml
+
+# Upload coverage results to codecov.
+./codecov upload-process
