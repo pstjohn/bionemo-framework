f3f6904fe1212938f02bb5528068da7035d2f2f7
Dorota Toczydlowska <115542912+dorotat-nv@users.noreply.github.com>
2025-03-14 13:32:11 +0100

    remove evo2 long context train test (#732)
    ### Description
    Marking as xfail unit tests
    :sub-packages/bionemo-evo2/tests/bionemo/evo2/run/test_train.py::test_train_single_gpu[7b_arc_longcontext]
    due to its issues on certain GPUs
    
    Issue as a follow-up:
    https://github.com/NVIDIA/bionemo-framework/issues/731
    
    ### Type of changes
    <!-- Mark the relevant option with an [x] -->
    
    - [x]  Bug fix (non-breaking change which fixes an issue)
    - [ ]  New feature (non-breaking change which adds functionality)
    - [ ]  Refactor
    - [ ]  Documentation update
    - [ ]  Other (please describe):
    
    ### CI Pipeline Configuration
    Configure CI behavior by applying the relevant labels:
    
    -
    [SKIP_CI](https://github.com/NVIDIA/bionemo-framework/blob/main/docs/docs/user-guide/contributing/contributing.md#skip_ci)
    - Skip all continuous integration tests
    -
    [INCLUDE_NOTEBOOKS_TESTS](https://github.com/NVIDIA/bionemo-framework/blob/main/docs/docs/user-guide/contributing/contributing.md#include_notebooks_tests)
    - Execute notebook validation tests in pytest
    -
    [INCLUDE_SLOW_TESTS](https://github.com/NVIDIA/bionemo-framework/blob/main/docs/docs/user-guide/contributing/contributing.md#include_slow_tests)
    - Execute tests labelled as slow in pytest for extensive testing
    
    
    > [!NOTE]
    > By default, the notebooks validation tests are skipped unless
    explicitly enabled.
    
    ### Usage
    <!--- How does a user interact with the changed code -->
    ```python
    TODO: Add code snippet
    ```
    
    ### Pre-submit Checklist
    <!--- Ensure all items are completed before submitting -->
    
     - [ ] I have tested these changes locally
     - [ ] I have updated the documentation accordingly
     - [ ] I have added/updated tests as needed
     - [ ] All existing tests pass successfully
    
    ---------
    
    Signed-off-by: Dorota Toczydlowska <115542912+dorotat-nv@users.noreply.github.com>

--- sub-packages/bionemo-evo2/tests/bionemo/evo2/run/test_train.py
+++ sub-packages/bionemo-evo2/tests/bionemo/evo2/run/test_train.py
@@ -130,8 +130,11 @@ def test_train_evo2_stops(tmp_path, num_steps=500000, early_stop_steps=3):
 
 
 @pytest.mark.slow
+@pytest.mark.parametrize(
+    "model_size",
+    ["1b_nv"],
+)
 @pytest.mark.skip(reason="This test requires a gpu larger than the 24Gb L4s available on GitHub Actions.")
-@pytest.mark.parametrize("model_size", ["7b_nv", "7b_arc_longcontext"])
 def test_train_single_gpu(tmp_path, model_size: str):
     """
     This test runs them single gpu evo2 training command with sample data in a temporary directory.
@@ -170,7 +173,7 @@ def test_train_single_gpu(tmp_path, model_size: str):
 
 @pytest.mark.slow
 @pytest.mark.distributed
-@pytest.mark.parametrize("model_size", ["7b_nv"])
+@pytest.mark.parametrize("model_size", ["1b_nv"])
 @pytest.mark.skip(
     reason="This tests requires to be run on a multi-gpu machine with torchrun --nproc_per_node=N_GPU -m pytest TEST_NAME"
 )
