459e0903f6b9718173d5bc277a74629e201df45e
Peter St. John <pstjohn@nvidia.com>
2025-09-16 10:19:05 -0700

    add golden value tests for esm-2
    Signed-off-by: Peter St. John <pstjohn@nvidia.com>

--- models/esm2/tests/test_thd_inputs.py
+++ models/esm2/tests/test_thd_inputs.py
@@ -39,46 +39,57 @@ def test_thd_from_collator_output(te_model_checkpoint, input_data_thd):
     assert outputs.loss < 3.0
 
 
-def test_thd_values_match(te_model_checkpoint, tokenizer, input_data, input_data_thd):
+def test_thd_values_match(te_model_checkpoint, tokenizer):
     # Manually masked input tokens so that both BSHD and THD models have the same mask pattern
+
+    proteins = [
+        "MLSATEKLSDYISSLFASVSIINSISTEDLFFLKLTCQTFSKDSEEYKAAYRILRGVQRGKVQIIEEALVS",
+        "MFVFFAGTLVNQDTLNFRDQLNINVVGTVRGIAQDASKYLEYAIDSV",
+        "MAATGSLILSDEEQAELIALAVRIVLACAGGSQNKELAAQLGVIETTVGEWRRRFAQNRVEGLRDEARPGAPSDDQ",
+        "MSAVLSAVASDDWTAFAKLVHPYVHWTADGITTRGRTRVMARLSGHDGVKPASSYELRDGQVYRWTS",
+        "MSDPAAEPPADTSGIAWRKSSYSGPNGNCVELAQISGDHVGIRNSRDLHGSVLTCTRAEFAALLCDIKAGRFDSLIL",
+        "MRRPKLRRSGVLMSHPARGQPIKDASTEAAAERRPHVTSSERQDVSDQDTR",
+        "MQTITVAGGNLFQIAAQYLGDATQWIRIAQLNGLADPVLSGVVTLTIPQPNPLAGGGVVGQ",
+        "MVFSLEQFVRGQGWQSITSNSDNEVPKPRQVYEVKAVCHPGAWRVKARVFGTSQGIPFDYSQASMERRVAQDECDRRPQ",
+        "AGDGTGCNPTLSKAAGVELDNSDSGEVFVIYLHIIIAIIVLISINLIGFLYF",
+        "MKVGVDPSVCEAHGACMSILPEVFDLDDDEVLQIRDGELAPSEEESAERAVASCPMGALRLSR",
+        "MWISERPPSRMALGSQSQMSLPGIPARCLHS",
+        "MIDNSIRLFDADDSELFSLAEVPLDNKPIQRDTDSLSQWGDTWLREIQHS",
+        "MVKNLFFNKIKNATLKVANISRCYLPFPPPPCPPPEPLEPPEPPAPLEPAPDPPPLPPFPVPDILPAI",
+        "MSYINDITQSNSSILNVNVKINDHNSDEMYRNETKWYGEQFRYQSNPRFSRSSTSKNEKGFVQKKT",
+        "MQILILPIPDQLQNPNKISQHLICITFVSEQTLPI",
+    ]
+
+    sequences = [tokenizer(protein) for protein in proteins]
     sequences = [
         {
-            "input_ids": [0, 32, 10, 15, 20, 1],
-            "attention_mask": [1, 1, 1, 1, 1, 1],
-            "labels": [-100, 5, -100, -100, -100, -100],
-        },
-        {
-            "input_ids": [0, 25, 32, 15, 1],
-            "attention_mask": [1, 1, 1, 1, 1],
-            "labels": [-100, -100, 8, -100, -100],
-        },
-        {
-            "input_ids": [0, 10, 12, 32, 18, 20, 22, 1],
-            "attention_mask": [1, 1, 1, 1, 1, 1, 1, 1],
-            "labels": [-100, -100, -100, 14, -100, -100, -100, -100],
-        },
+            "input_ids": seq["input_ids"],
+            "attention_mask": seq["attention_mask"],
+            "labels": seq["input_ids"],
+        }
+        for seq in sequences
     ]
 
     bhsd_collator = DataCollatorForTokenClassification(tokenizer=tokenizer, padding=True)
     thd_collator = DataCollatorWithFlattening(return_flash_attn_kwargs=True)
 
-    manual_input_data_bhsd = bhsd_collator(sequences)
-    manual_input_data_thd = thd_collator(sequences)
+    input_data_bhsd = bhsd_collator(sequences)
+    input_data_thd = thd_collator(sequences)
 
     model_bshd = NVEsmForMaskedLM.from_pretrained(te_model_checkpoint, dtype=torch.bfloat16)
     model_thd = NVEsmForMaskedLM.from_pretrained(te_model_checkpoint, attn_input_format="thd", dtype=torch.bfloat16)
     model_bshd.to("cuda")
     model_thd.to("cuda")
 
-    manual_input_data_bhsd = {k: v.to("cuda") for k, v in manual_input_data_bhsd.items()}
-    manual_input_data_thd = {
-        k: v.to("cuda") if isinstance(v, torch.Tensor) else v for k, v in manual_input_data_thd.items()
-    }
+    input_data_bhsd = {k: v.to("cuda") for k, v in input_data_bhsd.items()}
+    input_data_thd = {k: v.to("cuda") if isinstance(v, torch.Tensor) else v for k, v in input_data_thd.items()}
 
-    with torch.no_grad(), torch.amp.autocast("cuda", dtype=torch.bfloat16):
-        bshd_outputs = model_bshd(**manual_input_data_bhsd, output_hidden_states=True)
-        thd_outputs = model_thd(**manual_input_data_thd, output_hidden_states=True)
+    bshd_outputs = model_bshd(**input_data_bhsd)
+    thd_outputs = model_thd(**input_data_thd)
 
     print("bshd_outputs.loss", bshd_outputs.loss)
     print("thd_outputs.loss", thd_outputs.loss)
     torch.testing.assert_close(bshd_outputs.loss, thd_outputs.loss, atol=1e-2, rtol=1e-2)
+
+    bhsd_logits = bshd_outputs.logits[input_data_bhsd["attention_mask"].to(bool)]
+    torch.testing.assert_close(bhsd_logits, thd_outputs.logits, atol=1e-3, rtol=1e-3)
